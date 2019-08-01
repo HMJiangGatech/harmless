b5a119a36715653d77a64aec3c63349cbf843a2d
yxie98 <yxie98@gatech.edu>
2019-08-01 14:18:05 -0400

    delete test filw

--- main.py
+++ main.py
@@ -45,23 +45,20 @@ opt = parser.parse_args()
 seed = opt.seed
 np.random.seed(seed)
 torch.manual_seed(seed)
-    
-    
+
+
 def initialize(data, alpha0, K, T, method, device, lr, inner_lr, init_theta, read_checkpoint):
     G_matrix, tweets = data['matrix'], data['tweets']
     if read_checkpoint:
         with open(read_checkpoint, 'rb') as f:
             parameter = f.read()
-#            model_param = f.readlines()
-            
+
         parameter = pickle.loads(parameter)
         hawkes_models = parameter['Hawkes_models']
-#        hawkes_models = Hawkes_models(data,T,K,method, lr=lr, inner_lr=inner_lr, device=device, init = init_theta)
-#        hawkes_models.load_checkpoint(model_param)
-        
+
     else:
-        
-        parameter = {'alpha': None, 
+
+        parameter = {'alpha': None,
                      'gamma': None,
                      'phi': None,
                      'psi': None,
@@ -70,51 +67,39 @@ def initialize(data, alpha0, K, T, method, device, lr, inner_lr, init_theta, rea
                      'alpha0': alpha0,
                      'N': len(tweets),
                      'K': K}
-        
+
         N = len(tweets)
-    #    parameter['alpha'] = alpha0.copy()
         parameter['gamma'] = np.random.dirichlet(parameter['alpha0'], size=(N))
-    #    parameter['gamma'] = np.exp(np.random.normal(size=(N,K)))
-    #    parameter['gamma'] = parameter['gamma']/np.sum(parameter['gamma'], axis=-1, keepdims=True)
-        
         parameter['phi'] = np.random.dirichlet(parameter['alpha0'], size=(N, N))
-        
         parameter['psi'] = np.random.dirichlet(parameter['alpha0'], size=(N, N))
-        
+
         hawkes_models = Hawkes_models(data,T,K,method, lr=lr, inner_lr=inner_lr, device=device, init = init_theta)
-        
+
         phi_psi = np.expand_dims(parameter['phi'], axis=-1) * np.expand_dims(parameter['psi'], axis=-2)
         num_B = np.sum(phi_psi*np.expand_dims(np.expand_dims(G_matrix, axis=-1), axis=-1) , axis=(0,1))
-        
-    #    rho_num = np.sum((1-G_matrix)*np.sum(phi_psi, axis=(2,3)))
-    #    rho_don = np.sum(phi_psi)
+
         don_B = np.sum(phi_psi, axis=(0,1))
         parameter['B'] = (num_B/don_B).clip(1e-10, 1-1e-10)
-        
+
         parameter['rho'] = (1.-np.sum(G_matrix)/(N*N))
-  
+
     return parameter, hawkes_models
 
 
-if __name__ == '__main__':
-    
-#    PATH = '../twitter/'
-    device = 'cpu'
+main():
+
+    # GPU is slower, bacause the major optimization part does not involve large matrix computation
+    device = 'cpu' 
     pretrain_iter = opt.pretrain_iter
     num_iter = opt.max_iter
     K = opt.K
     alpha0 = np.ones(K)
     method = opt.method
-    
+
     print('Loading data...')
-    
-    if opt.data == 'twitter':
-        PATH = 'data/twitter'
-        G, tweets, val_tweets = load_data(PATH)
-        G_matrix = nx.to_numpy_matrix(G)
-        G_matrix = np.asarray(G_matrix) + np.eye(len(G_matrix))
-    elif opt.data == 'linkedin':
-        PATH = 'data/linkedin'  
+
+    if opt.data == 'linkedin':
+        PATH = 'data/linkedin'
         G, tweets, val_tweets = load_linkedin_data(PATH)
         G_matrix = nx.to_numpy_matrix(G)
         G_matrix = np.asarray(G_matrix)
@@ -130,128 +115,75 @@ if __name__ == '__main__':
         G_matrix = np.asarray(G_matrix) + np.eye(len(G_matrix))
     elif opt.data == '911':
         PATH = 'data/911'
-        G, tweets, val_tweets = read_911_data(PATH)
-        G_matrix = nx.to_numpy_matrix(G)
-        G_matrix = np.asarray(G_matrix) + np.eye(len(G_matrix))
-    elif opt.data == '911same':
-        PATH = 'data/911'
-        G, tweets, val_tweets = read_911_data_sameregion(PATH)
+        G, tweets, val_tweets = load_911_data(PATH)
         G_matrix = nx.to_numpy_matrix(G)
         G_matrix = np.asarray(G_matrix) 
 
     data = {'matrix': G_matrix, 'G': G, 'tweets': tweets, 'val_tweets': val_tweets }
-    
+
     print('Initializing...')
     parameter, hawkes_models = initialize(data, alpha0, K, 1, method, device, opt.lr, opt.inner_lr, opt.init_theta, opt.read_checkpoint)
-    
-    parameter = pretrain(data, parameter, pretrain_iter, opt.lr)
-    
-    
-#    parameter_list = []
-    loss_list = []
-    eval_list = []
-    eval_list_val = []
-    eval_list_test = []
-    #%%
-    for it in range(num_iter):
-        
-        print('Performing update', it)
-        
-        error_flag, loss = update_parameter(data, parameter, hawkes_models, opt.lr)
-        
-        if error_flag:
-            break
-        
-#        parameter_list.append(parameter.copy())
-        loss_list.append(loss)
-        
-        print('Iteration:', it, 'Loss:', loss)
-    
-        if it % 1 == 0:
-            hawkes_models.eval()
-            nll_val, nll_test, nll_all = hawkes_models.evaluate(parameter['gamma'])
-            eval_list.append(nll_all)
-            eval_list_val.append(nll_val)
-            eval_list_test.append(nll_test)
-            
-            print('Iteration:', it, 'Eval NLL:', nll_val)
-            
-            hawkes_models.train()
-    
-        
-    
-    #%%
-    
-#change this to your data    
-    if opt.data == 'linkedin':
-        delta_T_list = list(np.arange(1,11,1))
-        delta_T_list = list(np.arange(0.25,1,0.25))+delta_T_list
-        delta_T_list = [item/47.7753 for item in delta_T_list]
-    else:
-        delta_T_list = np.arange(0.025,0.2,0.025)
 
-    auc_list = []
-    fpr_list = []
-    tpr_list = []
-    for delta_T in delta_T_list:
-        auc, fpr, tpr = hawkes_models.get_roc_auc(parameter['gamma'], delta_T)
-        auc_list.append(auc)
-        fpr_list.append(fpr)
-        tpr_list.append(tpr)
+    parameter = pretrain(data, parameter, pretrain_iter, opt.lr)
 
     result_PATH = os.path.join(opt.result_path, 'our_result')
     if not os.path.isdir(result_PATH):
         os.makedirs(result_PATH)
-    
-    file_name = str(opt.data)+'_seed'+str(opt.seed)+'_lr'+str(opt.lr)+'_innerlr'+str(opt.inner_lr) \
+
+    temp_file_name = 'temp_' + str(opt.data)+'_seed'+str(opt.seed)+'_lr'+str(opt.lr)+'_innerlr'+str(opt.inner_lr) \
                 +'_K'+str(opt.K)+'_pretrain'+str(opt.pretrain_iter)+'_iter'+str(opt.max_iter) \
                 + '_method_'+opt.method+'_init_'+opt.init_theta+'.txt'
-    with open(os.path.join(result_PATH, file_name), 'w') as f:
-        for loss in loss_list:
-            f.write(str(loss)+', ')
-        f.write('\n')
-        for nll in eval_list:
-            f.write(str(nll)+', ')
-        f.write('\n')
-        for nll in eval_list_val:
-            f.write(str(nll)+', ')
-        f.write('\n')
-        for nll in eval_list_test:
-            f.write(str(nll)+', ')
-        f.write('\n')
-        f.write('delta_T\n')
-        for delta_T in delta_T_list:
-            f.write(str(delta_T)+', ')
-        f.write('\n')
-        f.write('auc\n')
-        for auc in auc_list:
-            f.write(str(auc)+', ')
-            
+
+    loss_list = []
+    eval_list = []
+    eval_list_val = []
+    eval_list_test = []
+
+    with open(os.path.join(result_PATH, temp_file_name), 'w') as f:
+        for it in range(num_iter):
+
+            print('Performing update', it)
+
+            error_flag, loss = update_parameter(data, parameter, hawkes_models, opt.lr)
+
+            if error_flag:
+                break
+
+            loss_list.append(loss)
+
+            print('Iteration:', it, 'Loss:', loss)
+
+            if it % 1 == 0:
+                hawkes_models.eval()
+                nll_val, nll_test, nll_all = hawkes_models.evaluate(parameter['gamma'])
+                eval_list.append(nll_all)
+                eval_list_val.append(nll_val)
+                eval_list_test.append(nll_test)
+
+                print('Iteration:', it, 'Eval NLL:', nll_val)
+
+
+                f.write(str(loss)+', ')
+                f.write(str(nll_all)+', ')
+                f.write(str(nll_val)+', ')
+                f.write(str(nll_test)+', ')
+                f.write('\n')
+
+                hawkes_models.train()
+
+
+
+
     checkpoint_PATH = os.path.join(opt.result_path, 'our_checkpoint')
     if not os.path.isdir(checkpoint_PATH):
         os.makedirs(checkpoint_PATH)
-        
+
     with open(os.path.join(checkpoint_PATH, file_name), 'wb') as f:
         parameter['Hawkes_models'] = hawkes_models
         f.write(pickle.dumps(parameter))
-        
-    
-    #%%
-    import matplotlib.pyplot as plt
-    
-    plt.plot(eval_list)
-    plt.show()
-    
-#%%    
 
-    for delta_T, fpr, tpr in zip(delta_T_list, fpr_list, tpr_list):
-        plt.plot(fpr, tpr, label='$\Delta T$='+str(delta_T))
-    plt.legend(fontsize=15)
-    plt.show()
-    
-    plt.plot(delta_T_list, auc_list)
-    plt.ylim([0,1])
-    plt.show()
+if __name__ == '__main__':
+    main()
 
-    
-    
+
+
--- utils.py
+++ utils.py
@@ -25,119 +25,9 @@ def plot_seq(tweets):
     plt.xlim([0,1])
     plt.show()
 
-def load_data(PATH):
-
-    with open(os.path.join(PATH, 'activity_all.txt'), 'r') as f:
-        data = f.readlines()
-
-    time_list = []
-    tweets = {}
-    for item in data:
-        time, user = item.strip('\n').split(' ')
-        if int(time)>1.482e9 and int(time)<=1.483e9:
-            time_list.append(int(time))
-            if user in tweets:
-                tweets[user].append(time)
-            else:
-                tweets[user] = [time]
-
-    min_time = np.min(time_list)
-    max_time = np.max(time_list) - min_time
-
-    for key in tweets:
-        tweets[key] = [(int(time)-min_time)/max_time for time in tweets[key]]
-
-
-
-
-    with open(os.path.join(PATH, 'network.txt'), 'r') as f:
-        network = f.readlines()
-
-    G = nx.Graph()
-
-    for item in network:
-        user1, user2 = item.strip('\n').split(' ')
-        G.add_edge(int(user1), int(user2))
-
-    #match the node number in the two dataset
-    #and remove empty sequence
-
-    for key in tweets:
-        if not G.has_node(int(key)):
-            G.add_node(int(key))
-
-    for node in list(G.nodes())[:]:
-        if str(node) not in tweets and G.has_node(node):
-            G.remove_node(node)
-        elif len(tweets[str(node)])<2 and G.has_node(node):
-            G.remove_node(node)
-
-    ########### make the gragh smaller #############
-    node_to_remove =[]
-    for node in G.nodes():
-        if node>200:
-            node_to_remove.append(node)
-    for node in node_to_remove:
-        G.remove_node(node)
-#    nx.draw(G)
-    ################################################
-
-    tweets_list = []
-    val_tweets_list = []
-    for node in G.nodes():
-        tweets_list.append(tweets[str(node)][:-1])
-        val_tweets_list.append(tweets[str(node)][-1])
-
-#    ############### scale the time line #################
-#    point_list = [time  for tweet in tweets_list for time in tweet]
-#
-#    num_bin = 60
-#    value, interval = np.histogram(point_list, bins=num_bin,range=(0,1))
-#
-#    scale = []
-#    prev = 0
-#    for item in value:
-#        new_scale = prev+item
-#        scale.append(new_scale)
-#        prev = new_scale
-#
-#    max_scale = scale[-1]
-#    scale = [item/max_scale for item in scale]
-#    scale.insert(0,0)
-#
-#    new_tweets_list = []
-#    for tweet in tweets_list:
-#        new_tweet = []
-#        for time in tweet:
-#            index = np.argmax(np.logical_and(interval>time-1./num_bin,interval<time))
-#            new_time = scale[index] + (time-interval[index])*num_bin *(scale[index+1]-scale[index])
-#            new_tweet.append(new_time)
-#        new_tweets_list.append(new_tweet)
-#
-#    new_val_tweets_list = []
-#    for time in val_tweets_list:
-#        index = np.argmax(np.logical_and(interval>time-1./num_bin,interval<time))
-#        new_time = scale[index] + (time-interval[index])*num_bin *(scale[index+1]-scale[index])
-#        new_val_tweets_list.append(new_time)
-#    ################################################
-#    point_list = [time  for tweet in new_tweets_list for time in tweet]
-#
-#    plt.hist(point_list, bins=num_bin)
-#    plt.show()
-#
-#
-#    length = []
-#    for tweet in new_tweets_list:
-#        length.append(len(tweet))
-#    print('mean length', np.mean(length), 'std length', np.std(length))
-#
-#    return G, new_tweets_list, new_val_tweets_list
-
-    return G, tweets_list, val_tweets_list
 
 
 def load_linkedin_data(PATH):
-#    PATH = '../data_preprocess/PoPPy/data/'
 
     with open(os.path.join(PATH, 'Linkedin.csv'), 'r') as f:
         data = f.readlines()
@@ -162,25 +52,6 @@ def load_linkedin_data(PATH):
     print('min_time', min_time, 'max_time', max_time)
     G = nx.Graph()
 
-    ## add edge according to be in the same company at the same time
-    #for user_i in seqs:
-    #    for user_j in seqs:
-    #        for p, org_i in enumerate(seqs_org[user_i]):
-    #            for q, org_j in enumerate(seqs_org[user_j]):
-    #                if org_i == org_j:
-    #                    if p==len(seqs_org[user_i])-1 and q==len(seqs_org[user_j])-1:
-    #                        G.add_edge(int(user_i), int(user_j))
-    #                    elif p==len(seqs_org[user_i])-1:
-    #                        if seqs[user_j][q+1]>seqs[user_i][p]:
-    #                            G.add_edge(int(user_i), int(user_j))
-    #                    elif q==len(seqs_org[user_j])-1:
-    #                        if seqs[user_i][p+1]>seqs[user_j][q]:
-    #                            G.add_edge(int(user_i), int(user_j))
-    #                    elif seqs[user_i][p]>seqs[user_j][q] and seqs[user_i][p]<seqs[user_j][q+1]:
-    #                        G.add_edge(int(user_i), int(user_j))
-    #                    elif seqs[user_j][q]>seqs[user_i][p] and seqs[user_j][q]<seqs[user_i][p+1]:
-    #                        G.add_edge(int(user_i), int(user_j))
-
     # add edge according to join the same company at the same time
     for user_i in seqs:
         for user_j in seqs:
@@ -197,15 +68,8 @@ def load_linkedin_data(PATH):
     for node in node_to_remove:
         G.remove_node(node)
 
-    ########### make the gragh smaller #############
-    for node in list(G.nodes()):
-        if node>1000:
-            G.remove_node(node)
-#    nx.draw(G)
-    ################################################
-
-    print(len(G.nodes()))
-    print(len(G.edges()))
+    print('Number of nodes:', len(G.nodes()))
+    print('Number of edges:',len(G.edges()))
 
     linkedin_list = []
     val_linkedin_list = []
@@ -215,7 +79,7 @@ def load_linkedin_data(PATH):
         val_linkedin_list.append(seqs[str(node)][-1])
         lenth_list.append(len(seqs[str(node)][:-1]))
 
-    print('length', np.mean(lenth_list), np.std(lenth_list))
+    print('Average length:', np.mean(lenth_list))
 
     new_linked_list = []
     new_val_linked_list = []
@@ -241,11 +105,15 @@ def load_overflow_data(PATH, isMath=True):
     for item in data:
         user1, user2, time = item.strip('\n').split(' ')
         user1, user2, time = int(user1), int(user2), int(time)
-        #if isMath or (time >= 0 and time <= 1.9e9):
-        if time >= 1.447e9 and time <= 1.449e9:
-        #stackoverflow
-        #if time >= 1.399e9 and time <= 1.9e9:
-        #mathoverflow
+
+        if isMath:
+            time_min = 1.399e9
+            time_max = 1.9e9
+        else:
+            time_min = 1.447e9
+            time_max = 1.449e9
+
+        if time >= time_min and time <= time_max:
             time_list.append(time)
             G.add_edge(user1, user2)
             if user1 in user_time_dict:
@@ -256,11 +124,8 @@ def load_overflow_data(PATH, isMath=True):
     min_time = np.min(time_list)
     max_time = np.max(time_list)
 
-    print(min_time)
-    print(max_time)
     max_time = np.max(time_list) - min_time
-    print(len(G.nodes()))
-    print(len(G.edges()))
+
     # normalize time and add node to G
     for key, val in user_time_dict.items():
         user_time_dict[key] = [(time-min_time)/max_time for time in val]
@@ -284,8 +149,9 @@ def load_overflow_data(PATH, isMath=True):
     for node in node_to_remove:
         G.remove_node(node) 
     assert(len(list(nx.isolates(G))) == 0)
-    print(len(G.nodes()))
-    print(len(G.edges()))
+
+    print('Number of nodes:', len(G.nodes()))
+    print('Number of edges:', len(G.edges()))
 
 
     seq_list = []
@@ -297,139 +163,7 @@ def load_overflow_data(PATH, isMath=True):
     return G, seq_list, val_seq_list
 
 
-def read_911_data(PATH):
-    G = nx.Graph()
-    path = os.path.join(PATH, '911.csv')
-
-    with open(path, 'r') as f:
-        data = f.readlines()
-    time_list = []
-    zip_time_dict = {}
-    for i in range(len(data)):
-        if i:
-            _,_,_,zip_code,_,time_str,_,_,_ = data[i].strip('\n').split(',')
-            if len(zip_code) == 5:
-                zip_code, timestamp = int(zip_code), time.mktime(datetime.datetime.strptime(time_str, "%m/%d/%y %H:%M").timetuple())
-                if timestamp >= 1.53e9 and timestamp <= 1.54e9:
-                    time_list.append(timestamp)
-                    if zip_code in zip_time_dict:
-                        zip_time_dict[zip_code].append(timestamp)
-                    else:
-                        zip_time_dict[zip_code] = [timestamp]
-
-    for zip_code1 in zip_time_dict:
-      for zip_code2 in zip_time_dict:
-        if (zip_code1 != zip_code2) and (np.abs(zip_code1 - zip_code2) <= 2):
-            G.add_edge(zip_code1, zip_code2)
-
-    min_time = np.min(time_list)
-    max_time = np.max(time_list)
-
-    print(min_time)
-    print(max_time)
-    abs_max_time = np.max(time_list) - min_time
-
-
-    # normalize time and add node to G
-    for key, val in zip_time_dict.items():
-        zip_time_dict[key] = [(time-min_time)/abs_max_time for time in val]
-        if not G.has_node(key) and (len(val) >= 2 and len(val) <= 7):
-            G.add_node(key)
-
-    # remove nodes in Graph
-    node_to_remove = []
-    for node in list(G.nodes()):
-        if node not in zip_time_dict and G.has_node(node):
-            node_to_remove.append(node)
-        elif len(zip_time_dict[node])  < 2 or len(zip_time_dict[node]) > 7 and G.has_node(node):
-            node_to_remove.append(node)
-    for node in node_to_remove:
-        G.remove_node(node)
-
-
-    seq_list = []
-    val_seq_list = []
-    for key, val in zip_time_dict.items():
-        if len(val) >= 2 and len(val) <= 7:
-            seq_list.append(val[:-1])
-            val_seq_list.append(val[-1])
-    return G, seq_list, val_seq_list
-
-# def read_911_data_sameregion(PATH):
-#     G = nx.Graph()
-#     path = os.path.join(PATH, '911.csv')
-
-#     with open(path, 'r') as f:
-#         data = f.readlines()
-#     time_list = []
-#     zip_time_dict = {}
-
-#     EMS = 'EMS'
-#     TRAFFIC = 'TRAFFIC'
-    
-
-#     for i in range(len(data)):
-#         if i > 1:
-#             _,_,_,zip_code, title,time_str,_,_,_ = data[i].strip('\n').split(',')
-#             if len(zip_code) == 5 and TRAFFIC in title:
-#                 zip_code, timestamp = int(zip_code), time.mktime(datetime.datetime.strptime(time_str, "%m/%d/%y %H:%M").timetuple())
-#                 if timestamp >= 0 and timestamp <= 1.59e9:
-#                     time_list.append(timestamp)
-#                     if zip_code in zip_time_dict:
-#                         zip_time_dict[zip_code].append(timestamp)
-#                     else:
-#                         zip_time_dict[zip_code] = [timestamp]
-                    
-
-
-#     for zip_code1 in zip_time_dict:
-#         for zip_code2 in zip_time_dict:
-#             if (zip_code1 != zip_code2) and (str(zip_code1)[:3] == str(zip_code2)[:3]):
-#                 G.add_edge(zip_code1, zip_code2)
-
-#     min_time = np.min(time_list)
-#     max_time = np.max(time_list)
-
-#     print(min_time)
-#     print(max_time)
-#     abs_max_time = np.max(time_list) - min_time
-
-
-#     # normalize time and add node to G
-#     for key, val in zip_time_dict.items():
-#         zip_time_dict[key] = [(time-min_time)/abs_max_time for time in val]
-#         if not G.has_node(key) and (len(val) >= 2 and len(val) <= 800):
-#             G.add_node(key)
-
-#     # remove nodes in Graph
-#     node_to_remove = []
-#     for node in list(G.nodes()):
-#         if node not in zip_time_dict and G.has_node(node):
-#             node_to_remove.append(node)
-#         elif len(zip_time_dict[node])  < 2 or len(zip_time_dict[node]) > 800 and G.has_node(node):
-#             node_to_remove.append(node)
-#     for node in node_to_remove:
-#         G.remove_node(node)
-
-#     # node_to_remove = set(list(nx.isolates(G)))
-#     # for node in node_to_remove:
-#     #     G.remove_node(node) 
-#     # assert(len(list(nx.isolates(G))) == 0)
-
-#     print(len(G.nodes()))
-#     print(len(G.edges()))
-
-
-#     seq_list = []
-#     val_seq_list = []
-#     for key, val in zip_time_dict.items():
-#         if len(val) >= 2 and len(val) <= 800:
-#             seq_list.append(val[:-1])
-#             val_seq_list.append(val[-1])
-#     return G, seq_list, val_seq_list
-
-
-def read_911_data_sameregion(PATH):
+def load_911_data(PATH):
     G = nx.Graph()
     path = os.path.join(PATH, '911.csv')
 
@@ -464,8 +198,6 @@ def read_911_data_sameregion(PATH):
     min_time = np.min(time_list)
     max_time = np.max(time_list)
 
-    print(min_time)
-    print(max_time)
     abs_max_time = np.max(time_list) - min_time
 
 
@@ -490,8 +222,8 @@ def read_911_data_sameregion(PATH):
         G.remove_node(node) 
     assert(len(list(nx.isolates(G))) == 0)
 
-    print(len(G.nodes()))
-    print(len(G.edges()))
+    print('Number of nodes:', len(G.nodes()))
+    print('Number of edges:', len(G.edges()))
 
 
     seq_list = []
